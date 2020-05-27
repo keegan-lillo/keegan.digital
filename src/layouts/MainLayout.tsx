682c01b96789f741c7f168c9143ac6fac8d8eb0b
Keegan Lillo <keegan@viv.ai>
2020-05-28 00:33:07 +1000

    Google fonts display=swap

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -60,7 +60,7 @@ export default function MainLayout({
         {/* === Device === */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..600&display=block"
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..600&display=swap"
           rel="stylesheet"
         />
       </Helmet>
